1110fead7686fd95919af65dcd62853ce378a23c
najaram <arambulo.jan@gmail.com>
2020-02-14 19:03:02 +0800

    update

--- lib/ds_watcher.rb
+++ lib/ds_watcher.rb
@@ -17,9 +17,39 @@ module DsWatcher
       true
     end
 
-    def display_recent
-      url = doc.css('a.o_chapter-container.disp-bl.color-off-black.hover-off-black.hover-bg-lighter-gray.flex').attribute('href').value
-      url
+    def display_chapter
+      if new_chapter?
+        text = []
+        coming_soon = doc.css('div.type-center.type-sm.line-caption.pad-y-rg.pad-y-md--lg.type-rg--lg').text.strip!
+
+        text << coming_soon
+        text << 'Here are some few chapters to re-read: '
+        text.join("\n")
+
+        text + recent_chapters
+      end
+    end
+
+    def recent_chapters
+      text = []
+      doc.css('a.o_chapter-container.disp-bl.color-off-black.hover-off-black.hover-bg-lighter-gray.flex').each do |chapter|
+        date_published = chapter.css('.pad-y-0.pad-r-0.pad-r-rg--sm').text
+        chapter_no = chapter.css('.disp-id.mar-r-sm').text
+        link = get_link(chapter.attribute('href').value)
+        text << date_published
+        text << chapter_no
+        text << link
+        text.join("\n")
+      end
+      text
+    end
+
+    def get_link(text)
+      if text.include? 'join to read'
+        'You should join first to read.'
+      else
+        text
+      end
     end
   end
 end
--- spec/ds_watcher_spec.rb
+++ spec/ds_watcher_spec.rb
@@ -6,17 +6,25 @@ RSpec.describe DsWatcher do
     expect(DsWatcher::VERSION).not_to be nil
   end
 
-  describe '#display_recent' do
-    it 'returns a url string' do 
+  describe '#display_chapter' do
+    it 'returns an array' do 
       @watcher = DsWatcher::Watcher.new
-      expect(@watcher.display_recent).to be_a String
+      expect(@watcher.display_chapter).to be_a Array
     end
   end
 
   describe '#new_chapter?' do
-    it 'returns boolean' do
+    it 'should return a boolean' do
       @watcher = DsWatcher::Watcher.new
       expect(@watcher.new_chapter?).to be(true).or(be(false))
+    end
+  end
+
+  describe '#get_link?' do
+    it 'should return a string' do
+      @watcher = DsWatcher::Watcher.new
+      result = @watcher.get_link('hello')
+      expect(result).to be_a String
     end
   end
 end
